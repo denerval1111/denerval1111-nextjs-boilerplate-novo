5ecd81ddcd6fb8aa59ce2f8b609d874433bd1b8c
denerval1111 <denerval1111@gmail.com>
2025-06-06 17:00:47 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 import './globals.css';
 import '../responsive.css';
 import '../animacoes.css';
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
+         <Navbar />
         {children}
         <Footer />
       </body>
