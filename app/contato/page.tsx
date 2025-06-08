ad4fbed5c89f78c5f43691e62469386304af0dd3
denerval1111 <denerval1111@gmail.com>
2025-06-08 05:32:39 -0300

    Update and rename page.tsx.bak to page.tsx

---
+++ app/contato/page.tsx
@@ -0,0 +1,8 @@
+export default function ContatoPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Página em Manutenção</h1>
+      <p>Nossa página de contato está temporariamente indisponível.</p>
+    </div>
+  );
+}
